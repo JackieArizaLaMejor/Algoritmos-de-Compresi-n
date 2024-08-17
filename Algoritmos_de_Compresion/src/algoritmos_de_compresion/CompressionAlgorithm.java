4fc4450d1c54c8bcc261f755a0c3927f05d27ef4
user <user@email.com>
2024-08-17 09:54:45 -0500

    correcion

--- Algoritmos_de_Compresion/src/algoritmos_de_compresion/CompressionAlgorithm.java
+++ Algoritmos_de_Compresion/src/algoritmos_de_compresion/CompressionAlgorithm.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package algoritmos_de_compresion;
 
-/**
- *
- * @author USUARIO
- */
 public class CompressionAlgorithm {
+    
+    public String compress(String filename){
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        return "CompressionAlgorithm{" + '}';
+    }
+    
+    
+    
+    
+    
+    
     
 }
