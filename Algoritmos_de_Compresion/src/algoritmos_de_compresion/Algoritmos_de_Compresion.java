c417e47bf83a98ee950978354c7e3f3b058d4304
xnocc <alonsomikoulits@gmail.com>
2024-08-17 09:58:27 -0500

    compresionmenu

--- Algoritmos_de_Compresion/src/algoritmos_de_compresion/Algoritmos_de_Compresion.java
+++ Algoritmos_de_Compresion/src/algoritmos_de_compresion/Algoritmos_de_Compresion.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package algoritmos_de_compresion;
 
-/**
- *
- * @author USUARIO
- */
+import java.util.Scanner;
+
+
 public class Algoritmos_de_Compresion {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+    CompresionMenu cm = new CompresionMenu();
+    cm.menu(sc);
     }
     
 }
--- Algoritmos_de_Compresion/src/algoritmos_de_compresion/CompresionMenu.java
+++ Algoritmos_de_Compresion/src/algoritmos_de_compresion/CompresionMenu.java
@@ -4,10 +4,31 @@
  */
 package algoritmos_de_compresion;
 
-/**
- *
- * @author USUARIO
- */
+
 public class CompresionMenu {
+    CompressionAlgorithm algorithm;
+    private void menus (Scanner sc) {
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("1) Para comprimir el archivo zip ");
+        System.out.println("2) Para comprimir el archivo rar ");
+        System.out.println("3) Para comprimir el archivo 7z");
+        int opc = Integer.parseInt(sc.nextLine());
+        System.out.println("Digite el nombre del archivo: ");
+        String filename = sc.nextLine();
+        
+        switch (opc){
+            case 1:
+                algorithm =new zip();
+                break;
+            case 2:
+                algorithm =new rar();
+                break;
+            case 3:
+                algorithm =new sietezip();
+                break;
+           
+        }
+        algorithm.compress(filename);
     
+}
 }
