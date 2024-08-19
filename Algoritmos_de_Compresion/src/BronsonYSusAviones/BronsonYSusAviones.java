0dfbdf0a9262303c904d38e0d84bb8c66a2e6bc0
xnocc <alonsomikoulits@gmail.com>
2024-08-18 22:01:38 -0500

    clase main y clase paquete

--- Algoritmos_de_Compresion/src/BronsonYSusAviones/BronsonYSusAviones.java
+++ Algoritmos_de_Compresion/src/BronsonYSusAviones/BronsonYSusAviones.java
@@ -10,7 +10,34 @@ package BronsonYSusAviones;
  */
 public class BronsonYSusAviones {
     public static void main(String[] args) {
+        Inventario in = new Inventario();
         
+        Pasajero av1 = new Pasajero();
+        av1.setMatricula("HK345");
+        av1.setVelocidad(300);
+        av1.setNumeroPasajeros(299);
+        Pasajero av2 = new Pasajero();
+        av2.setMatricula("HK346");
+        av2.setVelocidad(300);
+        av2.setNumeroPasajeros(150);
+        
+        Paquete p = new Paquete();
+        p.setNombre("Carta al niño Dios");
+        p.setPeso(255000.0);
+        c1.addPaquete(p);
+        Paquete p2 = new Paquete();
+        p.setNombre("Regalos");
+        p.setPeso(255000.2);
+        c1.addPaquete(p2);
+        Paquete p = new Paquete();
+        p.setNombre("Mirra");
+        p.setPeso(12000.0);
+        c1.addPaquete(p3);
+        
+        in.addAviones(av1);
+        in.addAviones(av1);
+        in.addAviones(c1);
+        in.listarAviones();
     }
     
 }
--- Algoritmos_de_Compresion/src/BronsonYSusAviones/Paquete.java
+++ Algoritmos_de_Compresion/src/BronsonYSusAviones/Paquete.java
@@ -4,10 +4,35 @@
  */
 package BronsonYSusAviones;
 
-/**
- *
- * @author pcgamer
- */
+
 public class Paquete {
+    private String nombre;
+    private double peso;
+
+    public Paquete(String nombre, double peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "nombre=" + nombre + ", peso=" + peso + '}';
+    }
     
 }
