d52fb660204b3846edd86f4dbc4aaea14b765f22
JackieArizaLaMejor <arizajackelin9@gmail.com>
2024-08-18 22:31:50 -0500

    Agregando el add en pasajero

--- Algoritmos_de_Compresion/src/BronsonYSusAviones/Carga.java
+++ Algoritmos_de_Compresion/src/BronsonYSusAviones/Carga.java
@@ -11,21 +11,22 @@ import java.util.ArrayList;
  * @author Yaquis
  */
 public class Carga extends Avion {
-    private ArrayList<Paquete> paquete = new ArrayList<>();
-    private double PESO_MAXIMO ;
+
+    private ArrayList<Paquete> paquetes = new ArrayList<>();
+    private double PESO_MAXIMO;
     private double pesoActual;
 
     public ArrayList<Paquete> getPaquete() {
-        return paquete;
+        return paquetes;
     }
-    
-    public void addPaquete(Paquete paquete){
+
+    public void addPaquete(Paquete paquete) {
+        paquetes.add(paquete);
     }
 
     @Override
     public String toString() {
-        return "Carga{" + "paquete=" + paquete + ", PESO_MAXIMO=" + PESO_MAXIMO + ", pesoActual=" + pesoActual + '}';
+        return "Carga{" + "paquete=" + paquetes + ", PESO_MAXIMO=" + PESO_MAXIMO + ", pesoActual=" + pesoActual + '}';
     }
-    
-    
+
 }
